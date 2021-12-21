a987ab6e76c99356d23b379b952ba6b7f176df8c
Ankit Prakash <ankitprakash@MTXs-MacBook-Pro.local>
2021-12-21 14:05:26 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,8 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    "mongodb://127.0.0.1:27017/",
+  port: 3030,
+  secret: "WPL_FINAL_PROJECT",
+};
